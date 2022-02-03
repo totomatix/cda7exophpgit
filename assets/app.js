9ede07168d6fa64866468a4261d0a32d7e714c0e
pagaric <pagaric@cwa12.com>
2022-02-03 17:01:08 +0100

    first commit

---
+++ assets/app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialisation de la sidenav
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+});
