97719353bc623ce772a7ade5aedc0260613098cf
pagaric <pagaric@cwa12.com>
2022-03-14 10:44:13 +0100

    exos 15 à 20

---
+++ exercices/exo18/index.php
@@ -0,0 +1,18 @@
+<?php ob_start(); //  ne pas modifier
+$titre = "Exo 15 - Tableaux"; // Mettre le titre de la page
+?>
+
+<!-- mettre votre code ici -->
+<?php
+
+
+?>
+
+<?php
+    /**
+     * Ne pa modifier
+     * permet d'inclure le menu et le template
+     */
+    $content = ob_get_clean(); 
+    require "../../partials/layout.php";
+?>
